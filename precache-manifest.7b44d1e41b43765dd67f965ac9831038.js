23aedced2b759445ef5285b64872e809e87a9cce
360gunner <hkentache@gmail.com>
2019-01-31 23:00:07 +0100

    Updates

---
+++ precache-manifest.7b44d1e41b43765dd67f965ac9831038.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Summary/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c55d06a4a94199b7d8a5cee452780ba8",
+    "url": "/Summary/static/media/Billabong.c55d06a4.woff"
+  },
+  {
+    "revision": "a9bce3fa84cd2c46cfaa",
+    "url": "/Summary/static/js/runtime~main.a9bce3fa.js"
+  },
+  {
+    "revision": "82f3e15806f35df53685",
+    "url": "/Summary/static/js/main.82f3e158.chunk.js"
+  },
+  {
+    "revision": "36c1a99720336c7010b5",
+    "url": "/Summary/static/js/1.36c1a997.chunk.js"
+  },
+  {
+    "revision": "82f3e15806f35df53685",
+    "url": "/Summary/static/css/main.c22183a9.chunk.css"
+  },
+  {
+    "revision": "36c1a99720336c7010b5",
+    "url": "/Summary/static/css/1.11520f59.chunk.css"
+  },
+  {
+    "revision": "78f32876aa3e696c8cd75678a77be823",
+    "url": "/Summary/index.html"
+  }
+];
